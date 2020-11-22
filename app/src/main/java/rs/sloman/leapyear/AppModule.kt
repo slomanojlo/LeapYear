1e90fe537281d0317417195f485f19b6320fd469
slomanojlo <slobodanmanojlovic@gmail.com>
2020-11-22 21:07:56 +0100

    Setup Koin

---
+++ app/src/main/java/rs/sloman/leapyear/AppModule.kt
@@ -0,0 +1,8 @@
+package rs.sloman.leapyear
+
+import org.koin.dsl.module
+
+@JvmField
+val appModule = module{
+
+}
---
+++ app/src/main/java/rs/sloman/leapyear/BaseApplication.kt
@@ -0,0 +1,20 @@
+package rs.sloman.leapyear
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidLogger()
+            androidContext(this@BaseApplication)
+            modules(appModule)
+        }
+    }
+
+}
