28930ad4fb7778440cfb87b95512d5aba4a3704b
slomanojlo <slobodanmanojlovic@gmail.com>
2020-11-29 17:00:24 +0100

    Refactor CheckLeapYearTest naming and parameters
    Library for JUnitParams added.
    Naming modified to comply to naming standards.
    Parameters extracted from class and scoped to test methods.
    Initial test method divided to two logical ones.

--- app/src/test/java/rs/sloman/leapyear/ChekLeapYearTest.kt
+++ app/src/test/java/rs/sloman/leapyear/ChekLeapYearTest.kt
@@ -1,42 +1,51 @@
 package rs.sloman.leapyear
 
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
 import org.junit.Test
 import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 
-@RunWith(Parameterized::class)
-class ChekLeapYearTest(
-    private val year: Int,
-    private val isLeapYear: Boolean,
-){
+@RunWith(JUnitParamsRunner::class)
+class ChekLeapYearTest {
 
     @Test
-    fun checkLeapYear() {
+    @Parameters(
+        value = [
+            "0",
+            "4",
+            "400",
+            "800",
+            "8",
+            "2000",
+            "4000",
+        ]
+    )
+    fun shouldReturnTrue_WhenYearIsLeap(year: Int) {
 
         val actualResult: Boolean = LeapYearCheck.isLeapYear(year)
 
-        assertEquals(isLeapYear, actualResult)
+        assertTrue(actualResult)
 
     }
 
-    companion object {
-        @JvmStatic
-        @Parameterized.Parameters(name = "{0} is leap year: {1}")
-        fun rounds() = listOf(
-            arrayOf(0, true),
-            arrayOf(1, false),
-            arrayOf(4, true),
-            arrayOf(400, true),
-            arrayOf(500, false),
-            arrayOf(600, false),
-            arrayOf(700, false),
-            arrayOf(800, true),
-            arrayOf(8, true),
-            arrayOf(2000, true),
-            arrayOf(2002, false),
-            arrayOf(4000, true),
-        )
+    @Test
+    @Parameters(
+        value = [
+            "500",
+            "600",
+            "700",
+            "2002",
+        ]
+    )
+    fun shouldReturnFalse_WhenYearIsNotLeap(year: Int) {
+
+        val actualResult: Boolean = LeapYearCheck.isLeapYear(year)
+
+        assertFalse(actualResult)
+
     }
+
 
 }
